dbe714a7ac0bc363fa8dee6526924ae6019b30fd
Steve Perkins <steve@steveperkins.com>
2017-02-14 11:10:09 -0500

    Initial commit

---
+++ src/main/java/com/steveperkins/config/SpringConfigSampleAppApplication.java
@@ -0,0 +1,12 @@
+package com.steveperkins.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigSampleAppApplication.class, args);
+	}
+}
